a76356a82a7a948e9e22d2a6e5b2366477af6e4a
ThomasVersaevel <Thomas0699@gmail.com>
2022-03-18 16:17:03 +0100

    added second line plot

--- js/simpage.js
+++ js/simpage.js
@@ -36,12 +36,14 @@ btn.addEventListener("click", startsim.bind(simparts))
 document.getElementById("stopbtn").addEventListener("click", tickStop.bind(simparts))
 //btn.addEventListener("click", ticker.start.bind(this))
 
+let customer = 0;
+
 function startsim() {
     if (this[0] != undefined && this[0].ticker != null) {
         this[0].ticker.destroy();
     }
     //this.hasEnded = true;
-    let customer = document.getElementById("parameter1").value;
+    customer = document.getElementById("parameter1").value;
     let shelves = document.getElementById("parameter2").value;
     let infrate = document.getElementById("parameter3").value
     this[0] = new Simulation(0, 20,20, shelves, customer, 0.1, infrate, 20, 1000, false, 1.0, true, this[1], 15);
@@ -51,8 +53,8 @@ function startsim() {
     //this.hasEnded = false;
     //this.runSimulation();
     var ticker = new PIXI.Ticker();
-ticker.add(tickUpdate.bind(this[0]), PIXI.UPDATE_PRIORITY.HIGH);
-ticker.start()
+    ticker.add(tickUpdate.bind(this[0]), PIXI.UPDATE_PRIORITY.HIGH);
+    ticker.start()
     this[0].ticker = ticker;
 }
 
--- js/visualisations.js
+++ js/visualisations.js
@@ -1,7 +1,6 @@
 //const {Chart} = require('chart.js');
 const ctx = document.getElementById('vis').getContext('2d');
-console.log(ctx);
-//ctx.fillRect(50, 50, 100, 100);
+const ctx2 = document.getElementById('vis2').getContext('2d');
 const myChart = new Chart(ctx, {
     type: 'line',
     data: {
@@ -20,6 +19,43 @@ const myChart = new Chart(ctx, {
             ],
             borderColor: [
                 'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        responsive: true,
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
+
+const myChart2 = new Chart(ctx2, {
+    type: 'line',
+    data: {
+        labels: ['8:00', '12:00', '14:00', '16:00', '18:00', '20:00'],
+        datasets: [{
+            fill: true,
+            label: 'Infections over time',
+            data: [11, 20, 7, 5, 5, 2],
+            backgroundColor: [
+                'rgba(99, 255, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(99, 255, 132, 1)',
                 'rgba(54, 162, 235, 1)',
                 'rgba(255, 206, 86, 1)',
                 'rgba(75, 192, 192, 1)',
