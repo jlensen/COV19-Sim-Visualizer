7e412e0d6471992e7713d5bcaa56400dbd1d52f5
ThomasVersaevel <thomas0699@gmail.com>
2022-03-26 11:20:34 +0100

    small changes

--- js/visualisations.js
+++ js/visualisations.js
@@ -1,6 +1,7 @@
 //const {Chart} = require('chart.js'); //already done in hmtl
+const canvas = document.getElementById('vis2');
+const ctx2 = canvas.getContext('2d');
 const ctx = document.getElementById('vis').getContext('2d');
-const ctx2 = document.getElementById('vis2').getContext('2d');
 const myChart = new Chart(ctx, {
     type: 'line',
     data: {
@@ -42,7 +43,7 @@ const myChart = new Chart(ctx, {
     }
 });
 
-const myChart2 = new Chart(ctx2, {
+const chart = new Chart(ctx2, {
     type: 'line',
     data: {
         labels: ['8:00', '12:00', '14:00', '16:00', '18:00', '20:00'],
@@ -71,7 +72,7 @@ const myChart2 = new Chart(ctx2, {
     },
     options: {
         responsive: true,
-        events: ['click'],
+        //events: ['click'],
         scales: {
             y: {
                 beginAtZero: true
@@ -79,6 +80,59 @@ const myChart2 = new Chart(ctx2, {
         }
     }
 });
-//myChart.resize(600, 600);
-// const chartImg = await Canvas.loadImage(myChart.toBase64Image());
-// ctx.drawImage(chartImg, 0, 0, canvas.width, canvas.height);  
+
+
+//var chart = new Chart(ctxOverlay, options);
+var overlay = document.getElementById('overlay');
+var startIndex = 0;
+overlay.width = canvas.width;
+overlay.height = canvas.height;
+var selectionContext = overlay.getContext('2d');
+var selectionRect = {
+  w: 0,
+  startX: 0,
+  startY: 0
+};
+var drag = false;
+canvas.addEventListener('pointerdown', evt => {
+  const points = chart.getElementsAtEventForMode(evt, 'index', {
+    intersect: false
+  });
+  startIndex = points[0]._index;
+  const rect = canvas.getBoundingClientRect();
+  selectionRect.startX = evt.clientX - rect.left;
+  selectionRect.startY = chart.chartArea.top;
+  drag = true;
+  // save points[0]._index for filtering
+});
+canvas.addEventListener('pointermove', evt => {
+
+  const rect = canvas.getBoundingClientRect();
+  if (drag) {
+    const rect = canvas.getBoundingClientRect();
+    selectionRect.w = (evt.clientX - rect.left) - selectionRect.startX;
+    selectionContext.globalAlpha = 0.5;
+    selectionContext.clearRect(0, 0, canvas.width, canvas.height);
+    selectionContext.fillRect(selectionRect.startX,
+      selectionRect.startY,
+      selectionRect.w,
+      chart.chartArea.bottom - chart.chartArea.top);
+  } else {
+    selectionContext.clearRect(0, 0, canvas.width, canvas.height);
+    var x = evt.clientX - rect.left;
+    if (x > chart.chartArea.left) {
+      selectionContext.fillRect(x,
+        chart.chartArea.top,
+        1,
+        chart.chartArea.bottom - chart.chartArea.top);
+    }
+  }
+});
+canvas.addEventListener('pointerup', evt => {
+
+  const points = chart.getElementsAtEventForMode(evt, 'index', {
+    intersect: false
+  });
+  drag = false;
+  console.log('implement filter between ' + options.data.labels[startIndex] + ' and ' + options.data.labels[points[0]._index]);  
+});
